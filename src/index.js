4c338fe622d99cbc0d527f640c7717b8f304ddb1
afeiship <1290657123@qq.com>
2021-05-17 13:55:14 +0800

    feat: add inteceptors/transform feature

--- src/index.js
+++ src/index.js
@@ -2,40 +2,69 @@
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var NxAbstractRequest = nx.AbstractRequest || require('@jswork/next-abstract-request');
+  var NxInterceptor = nx.Interceptor || require('@jswork/next-interceptor');
   var Taro = require('@tarojs/taro');
   var DEFAULT_OPTIONS = {
+    method: 'get',
     dataType: 'json',
-    responseType: 'text'
+    responseType: 'text',
+    interceptors: [],
+    transformRequest: nx.stubValue,
+    transformResponse: nx.stubValue
   };
 
   var NxTaroRequest = nx.declare('nx.TaroRequest', {
     extends: NxAbstractRequest,
     methods: {
+      init: function (inOptions) {
+        var parent = this.$base;
+        parent.init.call(this, inOptions);
+        this.interceptor = new NxInterceptor({ items: this.options.interceptors });
+      },
       defaults: function () {
         return DEFAULT_OPTIONS;
       },
-      request: function (inMethod, inUrl, inData, inOptions) {
+      request: function (inOptions) {
+        var self = this;
+        var options = this.interceptor.compose(inOptions, 'request');
+        options.data = options.transformRequest(options.data);
+        return new Promise(function (resolve, reject) {
+          self
+            .__request__(options)
+            .then(function (res) {
+              var composeRes = self.interceptor.compose(res, 'response');
+              var transformResponse = options.transformResponse(composeRes);
+              resolve(transformResponse);
+            })
+            .catch(reject);
+        });
+      },
+      __request__: function (inOptions) {
         var options = nx.mix(null, this.options, inOptions);
         return new Promise(function (resolve, reject) {
-          Taro.request(
-            nx.mix(
-              {
-                url: inUrl,
-                method: inMethod,
-                data: inData,
-                success: function (res) {
-                  resolve({ code: 0, detail: res.data, data: res });
+          try {
+            Taro.request(
+              nx.mix(
+                {
+                  url: inUrl,
+                  method: inMethod,
+                  data: inData,
+                  success: function (res) {
+                    resolve({ code: 0, detail: res.data, data: res });
+                  },
+                  fail: function (res) {
+                    resolve({ code: 1, detail: null, data: res });
+                  },
+                  complete: function (res) {
+                    resolve({ code: -1, detail: null, data: res });
+                  }
                 },
-                fail: function (res) {
-                  resolve({ code: 1, detail: null, data: res });
-                },
-                complete: function (res) {
-                  resolve({ code: -1, detail: null, data: res });
-                }
-              },
-              options
-            )
-          );
+                options
+              )
+            );
+          } catch (e) {
+            reject(e);
+          }
         });
       }
     }
