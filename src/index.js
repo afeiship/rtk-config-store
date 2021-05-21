5cede626c4c055aabf69ef9aefd5a767348fa391
afeiship <1290657123@qq.com>
2021-05-21 13:19:37 +0800

    fix: udpate text/json

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@
 
   var normalize = function (inOptions) {
     var headers = inOptions.headers;
+    var responseType = inOptions.responseType;
     headers['content-type'] = headers['Content-Type'];
     inOptions.data = inOptions.body;
     inOptions.header = headers;
     /**
      * 注意这里特别混淆
      * 1. 正常的 dataType: 是会决定 content-type + fetch(res.json()) 这两个条件的
-     * 2. 在 wx.request 中，responseType 只能是 text，所以这里不用设置
+     * 2. 在 wx.request 中，responseType 只能是 text/arraybuffer，所以这里不用设置
      */
-    inOptions.dataType = inOptions.responseType;
-    inOptions.responseType = 'text';
+    inOptions.responseType = responseType === 'json' ? 'text' : responseType;
     delete headers['Content-Type'];
     delete inOptions.headers;
     delete inOptions.body;
