6a9cedabc36f249313b64724a9736c26b053a507
afeiship <1290657123@qq.com>
2024-01-19 16:23:23 +0800

    Release 1.0.4

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,7 @@
 type Payload = Record<string, any>;
 
 interface NxStatic {
-  $patch: ((type: string, payload?: Payload) => any) & (target: { type: string; payload?: Payload }) => any;
+  $patch: ((type: string, payload?: Payload) => any) | ((target: { type: string; payload?: Payload }) => any);
   $use: ((path: any, defaults?) => any) | Function;
   $get: (path: string, defaults?) => any;
   store: import('@reduxjs/toolkit/dist/configureStore').ToolkitStore;
