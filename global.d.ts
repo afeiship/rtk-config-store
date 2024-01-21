19458c7e0c54eebf9c88161caee3d607f240dd92
afeiship <1290657123@qq.com>
2024-01-21 19:19:29 +0800

    fix:  params err

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,16 @@
 type Payload = Record<string, any>;
+type ActionObject = {
+  type: string;
+  payload?: any;
+};
+
+type ActionFunction = {
+  (type: string, payload?: any): ActionObject;
+  (action: ActionObject): ActionObject;
+};
 
 interface NxStatic {
-  $patch:
-    | ((type: string, payload?: Payload) => any)
-    | ((target: { type: string; payload?: Payload }) => any);
+  $patch: ActionFunction;
   $use: (path: any, defaults?) => any;
   $get: (path: string, defaults?) => any;
   store: import('@reduxjs/toolkit/dist/configureStore').ToolkitStore;
